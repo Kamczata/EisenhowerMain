a90796a6e5785e6759cbbbfeb90e0ca524a42f81
Kamczata <kam.czernek@gmail.com>
2021-12-06 18:11:25 +0100

    implemented AddItem and AddItemsFromFile and also IsUrgent methods

--- TodoMatrix.cs
+++ TodoMatrix.cs
@@ -1,10 +1,63 @@
 using System;
+using System.IO;
 
 namespace EisenhowerCore
 {
     public class TodoMatrix {
-
+    
+        public void AddItem(String title, DateTime deadline, bool isImportant)
+        {
+            bool isUrgent = IsUrgent(deadline);
+            if (isUrgent && isImportant)
+            {
+                TodoQuarters["IU"].ToDoItems.AddItem(title, deadline);
+            }
+            else if(isUrgent && !isImportant)
+            {
+                TodoQuarters["NU"].ToDoItems.AddItem(title, deadline);
+            }
+            else if(!isUrgent && isImportant)
+            {
+                TodoQuarters["IN"].ToDoItems.AddItem(title, deadline);
+            }
+            else if(!isUrgent && !isImportant)
+            {
+                TodoQuarters["NN"].ToDoItems.AddItem(title, deadline);
+            }
+        }
         
+        private bool IsUrgent(DateTime deadline)
+        {
+            DateTime today = DateTime.Today;
+            if ((deadline - today).TotalDays >= 3 )
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void AddItemsFromFile(string fileName)
+        {
+            string[] lines = System.IO.File.ReadAllLines(fileName);
+            foreach(string line in lines)
+            {
+                string[] item = line.Split('|');
+                string title = item[0];
+                string[] date = item[1].Split('-');
+                bool isImportant = new();
+                DateTime deadline = new DateTime(DateTime.Today.Year, Int32.Parse(date[1]), Int32.Parse(date[0]));
+                if (item[2]="")
+                {
+                    isImportant = false;
+                }
+                else
+                {
+                    isImportant = true;
+                }
+                this.AddItem(title, deadline, isImportant);
+                
+            }
+        }
     }
 
 }
